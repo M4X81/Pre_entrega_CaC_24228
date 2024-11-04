18024059df9b925facb3cc03cc83344ae8570b97
Maxi S <maxi0062009@hotmail.com>
2024-11-04 02:23:55 -0300

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const modal = document.getElementById('faqModal');
+        const openModalBtn = document.getElementById('openModalBtn');
+        const closeModalBtn = document.getElementById('closeModalBtn');
+
+        openModalBtn.onclick = () => {
+            modal.style.display = 'flex';
+        };
+
+        closeModalBtn.onclick = () => {
+            modal.style.display = 'none';
+        };
+
+        window.onclick = (event) => {
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        };
+
+        document.querySelectorAll('.faq-item h3').forEach(item => {
+            item.addEventListener('click', () => {
+                const pregunta = item.nextElementSibling;
+                pregunta.style.display = pregunta.style.display === 'none' ? 'block' : 'none';
+            });
+        });
