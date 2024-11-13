8e0fe8f7c29aa5ead33ab19b1149aba45555ec99
Maxi S <maxi0062009@hotmail.com>
2024-11-13 17:05:27 -0300

    agrgo alert a tienda, agregar redes despues

--- js/script.js
+++ js/script.js
@@ -1,29 +1,41 @@
-const modal = document.getElementById('faqModal');
-        const openModalBtn = document.getElementById('openModalBtn');
-        const closeModalBtn = document.getElementById('closeModalBtn');
+document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.getElementById('faqModal');
+    const openModalBtn = document.getElementById('openModalBtn');
+    const closeModalBtn = document.getElementById('closeModalBtn');
 
+    // Reviso primero que openModalBtn y modal existen
+    if (openModalBtn && modal) {
         openModalBtn.onclick = () => {
             modal.style.display = 'flex';
         };
+    }
 
+    if (closeModalBtn && modal) {
         closeModalBtn.onclick = () => {
             modal.style.display = 'none';
         };
+    }
 
-        window.onclick = (event) => {
-            if (event.target === modal) {
-                modal.style.display = 'none';
-            }
-        };
-
-        document.querySelectorAll('.faq-item h3').forEach(item => {
-            item.addEventListener('click', () => {
-                const pregunta = item.nextElementSibling;
-                pregunta.style.display = pregunta.style.display === 'none' ? 'block' : 'none';
-            });
-        });
-
-        function showAlert(event) {
-            event.preventDefault(); 
-            alert("Función aún no habilitada...");
+    // Cierra el modal
+    window.onclick = (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
         }
+    };
+
+    // Mostrar y ocultar respuestas en preguntas de FAQ
+    document.querySelectorAll('.faq-item h3').forEach(item => {
+        item.addEventListener('click', () => {
+            const pregunta = item.nextElementSibling;
+            pregunta.style.display = pregunta.style.display === 'none' ? 'block' : 'none';
+        });
+    });
+
+    // Agregar al carrito
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+    addToCartButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            alert('Función no disponible aún...');
+        });
+    });
+});
